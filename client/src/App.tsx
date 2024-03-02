100def8614daabf012685b62c59daf570d10f1b5
VladBrok <vlad.brok99@gmail.com>
2024-03-02 16:16:56 +0100

    configure client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,35 @@
-import './App.css'
+import styles from './App.module.css'
 
 function App(): JSX.Element {
-  return <div>Hello world</div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <div className={styles.headerContainer}>
+            <img
+              alt="logo"
+              height="90"
+              src="https://logux.org/branding/logo-light.svg"
+              width="90"
+            ></img>
+            <h1>Logux pagination example</h1>
+          </div>
+        </div>
+
+        <div>
+          <div>
+            <ol className={styles.instructionList}>
+              <li>Open this page in 2 different browsers</li>
+              <li>
+                Try updating, deleting or creating new players and see how pages
+                update in real-time
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
